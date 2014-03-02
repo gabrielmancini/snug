2242ec41e8858079eec5cf26130163a182364a88
Sven Lito <me@svenlito.com>
2014-03-02 21:17:09 +0000

    [minor] typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -135,7 +135,7 @@ module.exports = function (grunt) {
               exports: 'Backbone.hoodie',
               depends: {
                 backbone: 'Backbone',
-                hoodie: 'hoodie'
+                hoodie: 'Hoodie'
               }
             }
           }
